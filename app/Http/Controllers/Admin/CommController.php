595a92f7ab4a6cd6cdd38b2a138aeed8eac5afc8
Luooo <18219326208@163.com>
2019-06-25 17:52:01 +0800

    add files

---
+++ app/Http/Controllers/Admin/CommController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use App\Http\Controllers\Controller;
+
+class CommController extends Controller
+{
+    public function index(){
+        return "abc";
+    }
+}
